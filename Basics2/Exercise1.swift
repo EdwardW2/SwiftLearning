98ed2bd74623cbfef208dbbdbaa379548d0bcbbf
EdwardW2 <44901854+EdwardW2@users.noreply.github.com>
2022-08-03 16:45:09 +0100

    add excerise file

---
+++ Basics2/Exercise1.swift
@@ -0,0 +1,168 @@
+//
+//  Exercise1.swift
+//  Basics2
+//
+//  Created by Edward Wilkins on 03/08/2022.
+//
+
+import Foundation
+
+/*
+Scope: practise working with strings and arrays using control flows and operators
+ */
+class Homework1 {
+    
+    /*
+         Write an algorithm that prints a pyramid of this shape, where with each line the number of "x"s is increased by 1:
+         x
+         xx
+         xxx
+         xxxx
+         The max number of characters is the max count received as a parameter
+         
+         e.g.: maxCount = 6 gives this
+         x
+         xx
+         xxx
+         xxxx
+         xxxxx
+         xxxxxx
+         */
+        func printIncreasingPyramid1(maxCount: Int) {
+            
+        }
+    
+    /*
+         Write an algorithm that prints a pyramid of this shape, startig with the max number of "x"s and working your way down to 1:
+         xxxx
+         xxx
+         xx
+         x
+         The max count is the number of "x"s on the first line
+         
+         e.g.: maxCount = 6 gives this:
+         xxxxxx
+         xxxxx
+         xxxx
+         xxx
+         xx
+         x
+         */
+        func printDecreasingPyramid1(maxCount: Int) {
+            
+        }
+        
+        
+        /*
+         Write an algorithm that prints a pyramid of this shape, where with each line the number of "x"s is increased by 1:
+            x
+           xx
+          xxx
+         xxxx
+         The max number of characters is the max count received as a parameter
+         
+         e.g.: maxCount = 6 gives this
+              x
+             xx
+            xxx
+           xxxx
+          xxxxx
+         xxxxxx
+         */
+        func printIncreasingPyramid2(maxCount: Int) {
+            
+        }
+    
+    /*
+         Write an algorithm that prints a pyramid of this shape, startig with the max number of "x"s and working your way down to 1:
+         xxxx
+          xxx
+           xx
+            x
+         The max count is the number of "x"s on the first line
+         
+         e.g.: maxCount = 6 gives this:
+         xxxxxx
+          xxxxx
+           xxxx
+            xxx
+             xx
+              x
+         */
+        func printDecreasingPyramid2(maxCount: Int) {
+            
+        }
+        
+        /*
+         Write an algorithm that prints a half diamond of this shape, starting with 1, moving your way up to maxCount, then coming back down to 1:
+         x
+         xx
+         xxx
+         xxxx
+         xxx
+         xx
+         x
+         
+         The max count is the number of "x"s on in the middle
+         */
+        
+        func printHalfDiamond1(maxCount: Int) {
+            
+        }
+        
+        
+        /*
+         Write an algorithm that prints a half diamond of this shape, starting with 1, moving your way up to maxCount, then coming back down to 1:
+            x
+           xx
+          xxx
+         xxxx
+          xxx
+           xx
+            x
+         
+         The max count is the number of "x"s on in the middle
+         */
+        
+        func printHalfDiamond2(maxCount: Int) {
+            
+        }
+        
+        /*
+         Write an algorithm that decides which type of diamond should be printed (even or odd)
+         Hint: Use the remainder operator
+         */
+        func printDiamond(maxCount: Int) {
+            
+        }
+        
+        /*
+         Write an algorithm that prints a diamond of this shape, starting with 1, moving your way up to maxCount, then coming back down to 1:
+            xx
+           xxxx
+          xxxxxx
+         xxxxxxxx
+          xxxxxx
+           xxxx
+            xx
+         The max count is the number of "x"s on in the middle
+         */
+        private func printDiamond1(evenMaxCount: Int) {
+            
+        }
+        
+        /*
+         Write an algorithm that prints a diamond of this shape, starting with 1, moving your way up to maxCount, then coming back down to 1:
+            x
+           xxx
+          xxxxx
+         xxxxxxx
+          xxxxx
+           xxx
+            x
+         The max count is the number of "x"s on in the middle
+         */
+        private func printDiamond2(oddMaxCount: Int) {
+            
+        }
+}
